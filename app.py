a63676f6db454e6235fd9ab4e8feca73920f064e
Alan Lin <alanlin@Alans-MacBook-Air.local>
2023-06-27 16:52:02 -0700

    undid lost functionality

--- app.py
+++ app.py
@@ -24,16 +24,18 @@ def index():
     return render_template('index.html', password=password)
 
 @app.route('/get_password')
-def get_current_password():
+def get_password():
     read_password()
     return password
 
 @app.route('/update_password', methods=['POST'])
-def update_current_password():
+def update_password():
+    global password
     new_password = request.form.get('new_password')
     if new_password:
+        password = new_password.strip()
         with open(PASSWORD_FILE, 'w') as file:
-            file.write(new_password.strip())
+            file.write(password)
         return jsonify(success=True, message="Password updated successfully")
 
     return jsonify(success=False, message="Invalid request")
