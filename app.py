41f4103b97be02167af39e5cf1f96c8bd3366195
Alan Lin <alanlin@Alans-MacBook-Air.local>
2023-06-27 18:34:00 -0700

    refactored stuff

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
-import json
+import random
+from string import ascii_letters
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
@@ -8,10 +9,33 @@ PASSWORD_FILE = "password.txt"
 LIST_FILE = "list.json"
 MAX_LIST_SIZE = 35
 
-def check_password(password):
-    with open(PASSWORD_FILE, "r") as file:
-        stored_password = file.read().strip()
-    return password == stored_password
+def generate_password():
+    password = ''.join(random.choices(ascii_letters, k=6))
+    with open(PASSWORD_FILE, "w") as file:
+        file.write(password)
+
+def get_password():
+    if os.path.exists(PASSWORD_FILE):
+        with open(PASSWORD_FILE, "r") as file:
+            password = file.read().strip()
+        return password
+    else:
+        return ""
+
+def update_list(name):
+    if os.path.exists(LIST_FILE):
+        with open(LIST_FILE, "r") as file:
+            member_list = json.load(file)
+        for i, item in enumerate(member_list):
+            if item[1] == "":
+                member_list[i] = ("Performer", name)
+                break
+        else:
+            return False
+        with open(LIST_FILE, "w") as file:
+            json.dump(member_list, file)
+        return True
+    return False
 
 def get_member_list():
     if os.path.exists(LIST_FILE):
@@ -21,42 +45,58 @@ def get_member_list():
     else:
         return []
 
-def save_member_list(member_list):
-    with open(LIST_FILE, "w") as file:
-        json.dump(member_list, file)
+def clear_list():
+    if os.path.exists(LIST_FILE):
+        os.remove(LIST_FILE)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    message = None
-    member_list = get_member_list()
+    password = get_password()
+    return render_template("index.html", password=password)
 
-    if request.method == "POST":
-        password = request.form.get("password")
-        if check_password(password):
-            if len(member_list) < MAX_LIST_SIZE:
-                name = request.form.get("name")
-                if name:
-                    if name in member_list:
-                        message = "Name already exists in the list."
-                    else:
-                        member_list.append(name)
-                        save_member_list(member_list)
-                        return redirect(url_for("member"))
-                else:
-                    message = "Name field is empty."
-            else:
-                message = "The list is full. Cannot add more members."
-        else:
-            message = "Incorrect password. Please try again."
+@app.route("/get_password")
+def get_password_route():
+    password = get_password()
+    return jsonify(password)
 
-    return render_template("index.html", message=message, member_list=member_list, max_list_size=MAX_LIST_SIZE)
-
+@app.route("/securepasspage")
+def securepasspage():
+    password = get_password()
+    return render_template("securepasspage.html", password=password)
 
 @app.route("/member")
 def member():
     member_list = get_member_list()
     return render_template("member.html", member_list=member_list)
 
+@app.route("/secureadmin")
+def secureadmin():
+    member_list = get_member_list()
+    return render_template("secureadmin.html", member_list=member_list)
+
+@app.route("/update", methods=["POST"])
+def update():
+    name = request.form.get("name")
+    if name:
+        if update_list(name):
+            return redirect(url_for("member"))
+        else:
+            return "Sorry, there are no more available slots tonight."
+    return redirect(url_for("index"))
+
+@app.route("/randomize", methods=["POST"])
+def randomize():
+    member_list = get_member_list()
+    random.shuffle(member_list)
+    with open(LIST_FILE, "w") as file:
+        json.dump(member_list, file)
+    return redirect(url_for("secureadmin"))
+
+@app.route("/clear", methods=["POST"])
+def clear():
+    clear_list()
+    return redirect(url_for("secureadmin"))
 
 if __name__ == "__main__":
+    generate_password()
     app.run(debug=True)
